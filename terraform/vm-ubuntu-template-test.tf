fe0bc59a8e69697cb6b440c17fa5f7555935f4ea
DontDDoS <git@dontddos.me>
2026-01-24 14:08:17 +0000

    test

--- terraform/vm-ubuntu-template-test.tf
+++ terraform/vm-ubuntu-template-test.tf
@@ -2,23 +2,9 @@ resource "proxmox_vm_qemu" "ubuntu-template-test" {
   name        = "ubuntu-template-test"
   target_node = "pve-01"
   clone       = "ubuntu-template"
+  cores       = 2
+  memory      = 2048
+  sockets     = 1
+  agent       = 1
 
-  cores   = 2
-  memory  = 2048
-  sockets = 1
-  agent   = 1
-
-  disk {
-    storage = "disk-02"
-    size    = "20G"
-    type    = "scsi"
-  }
-
-  network {
-    model  = "virtio"
-    bridge = "vmbr1"
-  }
-
-  os_type   = "cloud-init"
-  ipconfig0 = "ip=dhcp"
 }
