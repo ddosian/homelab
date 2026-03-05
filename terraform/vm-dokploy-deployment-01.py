e4a670238e7d3a75969eb488c1d165890e7fa7f5
DontDDoS <git@dontddos.me>
2026-03-05 11:58:33 +0000

    created dokploy-deployment-01 vm

---
+++ terraform/vm-dokploy-deployment-01.py
@@ -0,0 +1,29 @@
+import utils
+
+# General Settings
+name = "dokploy-deployment-01"
+target_node = "pve-02"
+template = "ubuntu-24.04-template"
+
+# CPU Settings
+cpu_cores = 2
+cpu_sockets = 1
+cpu_type = "host"
+
+# RAM Settings
+ram_size = 8
+
+# Storage Settings
+disk_storage = "local-lvm"
+disk_size = 64
+cloudinit_storage = "local-lvm"
+
+# Network Settings
+network_bridge = "vmbr0"
+vlan_tag = 1
+ip_address = "10.77.1.110"
+cidr = "16"
+gateway = "10.77.0.3"
+
+with open("vm-dokploy-deployment-01.tf", "w") as f:
+    f.write(utils.generate_file(name, target_node, template, cpu_cores, cpu_sockets, cpu_type, ram_size, disk_storage, disk_size, cloudinit_storage, network_bridge, vlan_tag, ip_address, cidr, gateway))
